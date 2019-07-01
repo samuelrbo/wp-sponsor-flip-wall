4da9f8e5e27fc408fc05bae9b90ec75bb46b7c10
Samuel Ramon <samuelrbo@gmail.com>
2019-07-01 20:32:00 -0300

    Adding container to wrap sponsors' cards

--- view/wp_sfw_gallery.php
+++ view/wp_sfw_gallery.php
@@ -1,16 +1,20 @@
-<?php while(have_posts()): the_post(); ?>
+<div class="wp-sfw-container">
 
-<div class="wp-sfw-flip-card">
-  <div class="wp-sfw-flip-card-inner">
-    <div class="wp-sfw-flip-card-front">
-      <?php the_post_thumbnail( WP_SFW_THUMB ); ?>
-    </div>
+  <?php while(have_posts()): the_post(); ?>
 
-    <div class="wp-sfw-flip-card-back">
-      <?php the_title( '<h2>', '</h2>' ); ?>
-      <p><?php echo get_the_excerpt() ?></p>
+  <div class="wp-sfw-flip-card">
+    <div class="wp-sfw-flip-card-inner">
+      <div class="wp-sfw-flip-card-front">
+        <?php the_post_thumbnail( WP_SFW_THUMB ); ?>
+      </div>
+
+      <div class="wp-sfw-flip-card-back">
+        <?php the_title( '<h2>', '</h2>' ); ?>
+        <p><?php echo get_the_excerpt() ?></p>
+      </div>
     </div>
   </div>
-</div>
 
-<?php endwhile; ?>
+  <?php endwhile; ?>
+
+</div>
